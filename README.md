35a1baac01132d1f2eac07738fed2a186bbe83cc
curt caldwell and eliot charette <curt@>
2018-06-06 16:35:48 -0700

    Attempt to make a pig latin translator

---
+++ README.md
@@ -0,0 +1,57 @@
+# _Leap Year Project_
+
+#### _The application will translate English to Pig Latin, 06.06.18_
+
+#### By _**Curt Caldwell, Eliot Charette **_
+
+## Description
+
+_The program does nothing to non-alphabetical characters, since they do not contain consonants or vowels.
+Example Input: 3
+Example Output: 3
+Capitalization doesn't change regardless of order in a word or sentence.
+Example Input: Hi my name is Bob.
+Example Output: iHay ymay amenay isway obBay
+The program add “way” to the end of single vowel words.
+Example Input: a
+Example Output: away
+The program add “way” to the end of words beginning with a vowel.
+Example Input: apple
+Example Output: appleway
+For words beginning with one or more consonants the program will move all of the first consecutive consonants to the end, and add "ay.”
+Example Input: train
+Example Output: aintray
+If the first consonants include "qu", the program will move the "u" along with the "q" to the end and add “ay.”
+Example Input: queen
+Example Output: eenquay
+If the first consonants include "qu", the program will move the "u" along with the rest of the consonants to the end and add “ay.”
+Example Input: squeal
+Example Output: ealsquay
+For words beginning with "y", the program will treat "y" as a consonant.
+Example Input: yarn
+Example Output: arnyay
+The same rules apply in sentences as they do with single words.
+Example Input: Hi I like to eat apples.
+Example Output: iHay Iway ikelay otay eatway applesway.
+_
+
+## Setup/Installation Requirements
+
+* _Clone from GitHub_
+* _Copy and paste from a text editor_
+* _Open Html_
+
+
+## Support and contact details
+
+_For any questions please contact us at Curt.Caldwell24@gmail.com and EliotCharette@gmail.com_
+
+## Technologies Used
+
+_HTML, CSS, Bootstrap, Jquery, JavaScript, Atom, Terminal Command._
+
+### License
+
+*Licensed under MIT*
+
+Copyright (c) 2018 **_{Curt Caldwell Eliot Charette}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,86 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
+// business logic
+/*var translate = function(word) {
+    var inputLetters = word.split("");
+    var vowels = ["a", "e","i","o", "u"];
+    var position = vowels.indexOf(inputLetters[0]); //-1
 
-    var answers = ["person1", "person2", "animal", "exclamation", "verb", "noun", "guy"];
-/*
-forEach LOOP
-    answers.forEach(function(answer) {
-      var userInput = $("input#" + answer).val();
-      $("." + answer).text(userInput);
-    });
+    if (position > -1) {
+      //first letter vowel logic goes here
+      return word += "way";
+    } else {
+      //consonant first logic goes here
+      return   word += "ay";
+    }
+  }
 */
+/*
+  var pigify = function(word) {
 
-// FOR LOOP
-    for (i = 0; i < answers.length; i++) {
-      var userInput = $("input#" + answers[i]).val();
-      $("." + answers[i]).text(userInput);
+  var vowels = ["a", "e", "i", "o", "u"];
+
+  for (var i=0; i < word.length; i++) {
+    if ((vowels.indexOf(word[i])) != -1) {
+
+      if ((word[i] === "u") && (word[i-1] === "q")) {
+        var slice_point = i+1;
+      } else {
+        var slice_point = i;
+      }
+
+      var first_letters = word.slice(0, slice_point);
+      var remainder = word.slice(slice_point, word.length);
+      var piggy = remainder + first_letters + 'ay';
+
+      return piggy;
+    }
+  }
+};
+
+*/
+/*
+    for (var i = 0; i < word.length; i++){
+      if words.charAt(0) === vowels.indexOf()>= 0 {
+        words += ("way");
+      };
     };
-/*
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+      for (var i = 0; i < word.length; i++){
+        if word.charAt(0) !== ["a", "e","i","o", "u"]{
+          word.charAt(0).split().reverse();
+        }
+      }
+    };
+};
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
 */
 
-    $("#story").show();
-
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
     event.preventDefault();
+    //var year = $("input#year").val();
+    var result = pigify ($("input#year").val());
+    debugger;
+    function(word) {
+
+    var vowels = ["a", "e", "i", "o", "u"];
+
+    for (var i=0; i < word.length; i++) {
+      if ((vowels.indexOf(word[i])) != -1) {
+
+        if ((word[i] === "u") && (word[i-1] === "q")) {
+          var slice_point = i+1;
+        } else {
+          var slice_point = i;
+        }
+
+        var first_letters = word.slice(0, slice_point);
+        var remainder = word.slice(slice_point, word.length);
+        var piggy = remainder + first_letters + 'ay';
+
+        return piggy;
+      }
+    }
+  });
+    $("#result").show(piggy);
   });
 });
